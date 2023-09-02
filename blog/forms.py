b2f9b0eee07349982f5cee9812b1e30b7bab0c0b
Charliedev-11095 <carlosroyiga@gmail.com>
2023-09-02 16:50:39 -0600

    último cambio

--- blog/forms.py
+++ blog/forms.py
@@ -4,5 +4,12 @@ from .models import Post
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'text',)
+        fields = ('author', 'title', 'text', 'published_date')
 
+    # Agrega clases de Bootstrap a los widgets
+    widgets = {
+        'author': forms.Select(),
+        'title': forms.TextInput(),
+        'text': forms.Textarea(attrs={'class': 'form-control'}),
+        'published_date': forms.DateTimeInput(attrs={'class': 'form-control'}),
+    }
--- blog/models.py
+++ blog/models.py
@@ -4,8 +4,8 @@ from django.utils import timezone
 
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
-    text = models.TextField()
+    title = models.CharField(("Titulo"),max_length=200)
+    text = models.TextField(("Texto "),max_length=1000)
     created_date = models.DateTimeField(
             default=timezone.now)
     published_date = models.DateTimeField(

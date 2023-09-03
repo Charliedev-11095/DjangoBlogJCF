212b93534fb6dcf26d26b99d3a21010e696d1614
Charliedev-11095 <carlosroyiga@gmail.com>
2023-09-02 18:01:02 -0600

    bien

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,6 @@ from .forms import PostForm
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseNotFound
 
-@login_required
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
